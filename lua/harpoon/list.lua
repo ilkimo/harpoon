9842b24e97e88479061d75c77f0b2ef5d5c380ab
il_kimo <vibertikim@yahoo.it>
2025-01-30 23:24:20 +0100

    test: add debug sus

--- lua/harpoon/list.lua
+++ lua/harpoon/list.lua
@@ -343,7 +343,7 @@ function HarpoonList:display()
     for i = 1, self._length do
         local v = self.items[i]
         if v == nil then
-            out[i] = ""
+            out[i] = "sus"
         else
             out[i] = self.config.display(v)
         end
